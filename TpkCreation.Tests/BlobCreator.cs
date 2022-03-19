91c03ed9a874fcd53ffacd2c31d6ae396929265d
Jeremy Pritts <jrpritts7@gmail.com>
2022-03-19 02:56:30 -0400

    make a blob creator test class

---
+++ TpkCreation.Tests/BlobCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace AssetRipper.TpkCreation.Tests
+{
+	internal static class BlobCreator
+	{
+		public static TpkFileSystemBlob MakeRandomFileSystemBlob()
+		{
+			TpkFileSystemBlob blob = new TpkFileSystemBlob();
+			blob.AddRandomEntries(10);
+			return blob;
+		}
+
+		private static void AddRandomEntries(this TpkFileSystemBlob blob, int count)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				string name = RandomUtils.RandomString();
+				byte[] data = RandomUtils.RandomBytes();
+				blob.Files.Add(new KeyValuePair<string, byte[]>(name, data));
+			}
+		}
+	}
+}
--- TpkCreation.Tests/FileSystemTests.cs
+++ TpkCreation.Tests/FileSystemTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System.Collections.Generic;
 
 namespace AssetRipper.TpkCreation.Tests
 {
@@ -8,27 +7,10 @@ namespace AssetRipper.TpkCreation.Tests
 		[Test]
 		public void ReadAndWriteAreTheSame()
 		{
-			TpkFileSystemBlob originalBlob = MakeRandomBlob();
+			TpkFileSystemBlob originalBlob = BlobCreator.MakeRandomFileSystemBlob();
 			byte[] writtenData = originalBlob.ToBinary();
 			TpkFileSystemBlob readBlob = TpkDataBlob.FromBinary<TpkFileSystemBlob>(writtenData);
 			Assert.AreEqual(originalBlob.Files, readBlob.Files);
-		}
-
-		private static TpkFileSystemBlob MakeRandomBlob()
-		{
-			TpkFileSystemBlob blob = new TpkFileSystemBlob();
-			AddRandomEntries(blob, 10);
-			return blob;
-		}
-
-		private static void AddRandomEntries(TpkFileSystemBlob blob, int count)
-		{
-			for(int i = 0; i < count; i++)
-			{
-				string name = RandomUtils.RandomString();
-				byte[] data = RandomUtils.RandomBytes();
-				blob.Files.Add(new KeyValuePair<string, byte[]>(name, data));
-			}
 		}
 	}
 }
