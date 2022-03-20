720b095fb3796b85340a5039a889121407271983
Jeremy Pritts <jrpritts7@gmail.com>
2022-03-20 04:29:33 -0400

    compression tests

---
+++ TpkCreation.Tests/CompressionTests.cs
@@ -0,0 +1,18 @@
+﻿using AssetRipper.TpkCreation.Compression;
+using NUnit.Framework;
+
+namespace AssetRipper.TpkCreation.Tests
+{
+	public class CompressionTests
+	{
+		private readonly byte[] randomData = RandomUtils.RandomBytes(4096);
+
+		[Test]
+		public void Lz4CompressionReversesPerfectly()
+		{
+			byte[] compressedData = Lz4Handler.Compress(randomData);
+			byte[] decompressedData = Lz4Handler.Decompress(compressedData, randomData.Length);
+			Assert.AreEqual(randomData, decompressedData);
+		}
+	}
+}
