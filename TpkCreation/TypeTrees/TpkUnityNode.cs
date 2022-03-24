e0527351869ae79927bdb665ace6c4d9e2ed00f7
Jeremy Pritts <jrpritts7@gmail.com>
2022-03-24 16:42:19 -0400

    move child indices to node data

--- TpkCreation/TypeTrees/TpkUnityNode.cs
+++ TpkCreation/TypeTrees/TpkUnityNode.cs
@@ -5,29 +5,15 @@ namespace AssetRipper.TpkCreation.TypeTrees
 	public sealed class TpkUnityNode
 	{
 		public ushort NodeData { get; set; }
-		public TpkUnityNode[] SubNodes { get; set; } = Array.Empty<TpkUnityNode>();
 
 		public void Read(BinaryReader reader)
 		{
 			NodeData = reader.ReadUInt16();
-			int count = reader.ReadInt32();
-			SubNodes = new TpkUnityNode[count];
-			for (int i = 0; i < count; i++)
-			{
-				SubNodes[i] = new TpkUnityNode();
-				SubNodes[i].Read(reader);
-			}
 		}
 
 		public void Write(BinaryWriter writer)
 		{
 			writer.Write(NodeData);
-			int count = SubNodes.Length;
-			writer.Write(count);
-			for (int i = 0;i < count; i++)
-			{
-				SubNodes[i].Write(writer);
-			}
 		}
 
 		public static TpkUnityNode Convert(UnityNode node, TpkStringBuffer stringBuffer, TpkUnityNodeDataBuffer nodeBuffer)
@@ -39,15 +25,27 @@ namespace AssetRipper.TpkCreation.TypeTrees
 			nodeData.Version = node.Version;
 			nodeData.TypeFlags = node.TypeFlags;
 			nodeData.MetaFlag = node.MetaFlag;
+
+			int subNodeCount = node.SubNodes.Count;
+			nodeData.SubNodes = subNodeCount == 0 ? Array.Empty<ushort>() : new ushort[subNodeCount];
+			for (int i = 0; i < subNodeCount; i++)
+			{
+				nodeData.SubNodes[i] = Convert(node.SubNodes[i], stringBuffer, nodeBuffer).NodeData;
+			}
+
 			TpkUnityNode result = new TpkUnityNode();
 			result.NodeData = nodeBuffer.AddNode(nodeData);
-			result.SubNodes = node.SubNodes.Select(n => Convert(n, stringBuffer, nodeBuffer)).ToArray();
 			return result;
 		}
 
 		public static UnityNode Convert(TpkUnityNode node, TpkStringBuffer stringBuffer, TpkUnityNodeDataBuffer nodeBuffer, byte level, int index, out int lastIndexUsed)
 		{
 			TpkUnityNodeData nodeData = nodeBuffer[node.NodeData];
+			return Convert(nodeData, stringBuffer, nodeBuffer, level, index, out lastIndexUsed);
+		}
+
+		public static UnityNode Convert(TpkUnityNodeData nodeData, TpkStringBuffer stringBuffer, TpkUnityNodeDataBuffer nodeBuffer, byte level, int index, out int lastIndexUsed)
+		{
 			UnityNode result = new UnityNode();
 			result.TypeName = stringBuffer[nodeData.TypeName];
 			result.Name = stringBuffer[nodeData.Name];
@@ -60,11 +58,11 @@ namespace AssetRipper.TpkCreation.TypeTrees
 
 			byte levelPlus = unchecked((byte)(level + 1U));
 			lastIndexUsed = index;
-			int subNodeCount = node.SubNodes.Length;
+			int subNodeCount = nodeData.SubNodes.Length;
 			result.SubNodes = new List<UnityNode>(subNodeCount);
 			for(int i = 0; i < subNodeCount; i++)
 			{
-				result.SubNodes.Add(Convert(node.SubNodes[i], stringBuffer, nodeBuffer, levelPlus, index + 1, out lastIndexUsed));
+				result.SubNodes.Add(Convert(nodeBuffer[nodeData.SubNodes[i]], stringBuffer, nodeBuffer, levelPlus, index + 1, out lastIndexUsed));
 				index = lastIndexUsed;
 			}
 
--- TpkCreation/TypeTrees/TpkUnityNodeData.cs
+++ TpkCreation/TypeTrees/TpkUnityNodeData.cs
@@ -10,6 +10,7 @@
 		public short Version { get; set; }
 		public byte TypeFlags { get; set; }
 		public uint MetaFlag { get; set; }
+		public ushort[] SubNodes { get; set; } = Array.Empty<ushort>();
 
 		public void Read(BinaryReader reader)
 		{
@@ -19,6 +20,12 @@
 			Version = reader.ReadInt16();
 			TypeFlags = reader.ReadByte();
 			MetaFlag = reader.ReadUInt32();
+			ushort count = reader.ReadUInt16();
+			SubNodes = new ushort[count];
+			for (int i = 0; i < count; i++)
+			{
+				SubNodes[i] = reader.ReadUInt16();
+			}
 		}
 
 		public void Write(BinaryWriter writer)
@@ -29,6 +36,11 @@
 			writer.Write(Version);
 			writer.Write(TypeFlags);
 			writer.Write(MetaFlag);
+			writer.Write((ushort)SubNodes.Length);
+			for (int i = 0; i < SubNodes.Length; i++)
+			{
+				writer.Write(SubNodes[i]);
+			}
 		}
 
 		public override bool Equals(object? obj)
@@ -49,7 +61,22 @@
                 ByteSize == other.ByteSize &&
                 Version == other.Version &&
                 TypeFlags == other.TypeFlags &&
-                MetaFlag == other.MetaFlag;
+                MetaFlag == other.MetaFlag &&
+				ArrayEqual(SubNodes, other.SubNodes);
+		}
+
+		private static bool ArrayEqual(ushort[] array1, ushort[] array2)
+		{
+			if (array1.Length != array2.Length)
+				return false;
+			for (int i = 0; i < array1.Length; i++)
+			{
+				if (array1[i] != array2[i])
+				{
+					return false;
+				}
+			}
+			return true;
 		}
 	}
 }
