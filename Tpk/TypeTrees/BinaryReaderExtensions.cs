2829ef4ea6fff6527627cd764dae20e2bd9ba664
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-07-10 16:04:54 -0400

    Add extension methods for BinaryReader and BinaryWriter

---
+++ Tpk/TypeTrees/BinaryReaderExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace AssetRipper.Tpk.TypeTrees;
+
+/// <summary>
+/// Unity version extension methods for <see cref="BinaryReader"/>
+/// </summary>
+internal static class BinaryReaderExtensions
+{
+	/// <summary>
+	/// Reads a Unity version from the stream
+	/// </summary>
+	/// <param name="reader">A binary reader</param>
+	/// <returns>The read Unity version</returns>
+	public static UnityVersion ReadUnityVersion(this BinaryReader reader)
+	{
+		return UnityVersion.FromBits(reader.ReadUInt64());
+	}
+}
---
+++ Tpk/TypeTrees/BinaryWriterExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace AssetRipper.Tpk.TypeTrees;
+
+/// <summary>
+/// Unity version extension methods for <see cref="BinaryWriter"/>
+/// </summary>
+internal static class BinaryWriterExtensions
+{
+	/// <summary>
+	/// Write a Unity version to the stream
+	/// </summary>
+	/// <param name="writer">A binary writer</param>
+	/// <param name="version">A Unity version</param>
+	public static void Write(this BinaryWriter writer, UnityVersion version)
+	{
+		writer.Write(version.GetBits());
+	}
+}
